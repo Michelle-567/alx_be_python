369010e87afbeaccdfea614f22882ec741364a93
Michelle-567 <michellendirangu2001@gmail.com>
2025-06-19 21:40:31 +0300

    add book_class.py, main.py, library_system.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,20 +7,22 @@
 #Destructor (__del__): Prints "Deleting (title of the book)" upon object deletion.
 #String Representation (__str__): Returns a string in the format "(title) by (author), published in (year)".
 #Official Representation (__repr__): Returns a string that would recreate the Book instance: f"Book('{self.title}', '{self.author}', {self.year})".
+# book_class.py
+
 class Book:
-    def __init__(self,title,author,year):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
     def __str__(self):
-        return f"{self.title} {self.author} {self.year}"
-      
-    
+        return f"{self.title} by {self.author}, published in {self.year}"
+
     def __repr__(self):
-        return f"Book('{self.title}','{self.author}',{self.year})"    
-    
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
     def __del__(self):
         print(f"Deleting {self.title}")
+
 
     
